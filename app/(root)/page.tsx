2405949e40cd7c4e406e2766fd1696ba79ef37be
Tihomir Bogajcevic <tihomir.bogajcevic@symphony.is>
2024-10-30 14:32:39 +0100

    Card UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
@@ -6,6 +7,21 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>; //https://nextjs.org/docs/app/api-reference/file-conventions/page#searchparams-optional
 }) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Jos" },
+      _id: 1,
+      description: "The best season so far.",
+      image:
+        "https://media.formula1.com/image/upload/t_16by9North/f_auto/q_auto/v1706626658/fom-website/2023/Miscellaneous/GettyImages-1656999898.jpg",
+      category: "Racing",
+      title: "Formula 1",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -17,6 +33,22 @@ export default async function Home({
         </p>
 
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No results found.</p>
+          )}
+        </ul>
       </section>
     </>
   );
