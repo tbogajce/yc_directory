1f72fce6f8612a230fe18b49d9ffc7ae9f9d86c4
Tihomir Bogajcevic <tihomir.bogajcevic@symphony.is>
2024-11-01 17:07:51 +0100

    Server action for creating a pitch

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -10,6 +10,7 @@ import { formSchema } from "@/lib/validation";
 import { z } from "zod";
 import { useToast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
+import { createPitch } from "@/lib/actions";
 
 function StartupForm() {
   const [errors, setErrors] = useState<Record<string, string>>({});
@@ -31,18 +32,18 @@ function StartupForm() {
 
       await formSchema.parseAsync(formValues);
 
-      // const result = await createIdea(prevState, formData, pitch);
+      const result = await createPitch(prevState, formData, pitch);
 
-      // if (result.status == "SUCCESS") {
-      //   toast({
-      //     title: "Success",
-      //     description: "Your startup pitch has been created successfully",
-      //   });
+      if (result.status == "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup pitch has been created successfully",
+        });
 
-      //   router.push(`/startup/${result._id}`);
-      // }
+        router.push(`/startup/${result._id}`);
+      }
 
-      // return result
+      return result;
     } catch (error) {
       if (error instanceof z.ZodError) {
         const fieldErorrs = error.flatten().fieldErrors;
